cd16d2d456024975b1eceb926bc8bf6a33437dfb
Tony Teague <tst722@nyu.edu>
2017-03-20 11:50:45 -0400

    NYU Inventory

---
+++ mysql/nyu.inventory.sql
@@ -0,0 +1,58 @@
+create database ServerInv; 
+CREATE TABLE IF NOT EXISTS ServerTable (
+     serverID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     siteCode CHAR()       NOT NULL DEFAULT '',    
+     countryCode CHAR(3)   NOT NULL DEFAULT '',
+	 regionCode  CHAR(3)   NOT NULL DEFAULT '',
+	 datacenter  VARCHAR(30)   NOT NULL DEFAULT '',
+	 floor       INT UNSIGNED  NOT NULL DEFAULT 1,
+	 cage        VARCHAR(10)   NOT NULL DEFAULT '',
+	 cabinet     VARCHAR(10)   NOT NULL DEFAULT '',
+	 rackunit    VARCHAR(10)   NOT NULL DEFAULT '',
+	 serverName  VARCHAR(30)   NOT NULL DEFAULT '',
+	 serverOS    VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverENV   VARCHAR(3)    NOT NULL DEFAULT '',    
+	 serverRelease  VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverHardware VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverMgmt     VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverSerial   VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverAssettag VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverVendor   VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverMemory   VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverCPU      VARCHAR(20)   NOT NULL DEFAULT '',
+	 serverSAN      VARCHAR(1)    NOT NULL DEFAULT '',
+	 serverNAS      VARCHAR(1)    NOT NULL DEFAULT '',
+	 serverBackup   VARCHAR(1)    NOT NULL DEFAULT '',  
+	 supportgroup   VARCHAR(20)   NOT NULL DEFAULT '',
+	 businessunit   VARCHAR(20)   NOT NULL DEFAULT '',
+	 serviceDate    VARCHAR(20)   NOT NULL DEFAULT '',
+	 decomDate VARCHAR(20)   NOT NULL DEFAULT '',
+     PRIMARY KEY (serverID)
+     );
+	 
+	 
+create database NetworkCCInv; 
+CREATE TABLE IF NOT EXISTS NetworkCCTable (
+     networkCCID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     siteCode       CHAR(3)       NOT NULL DEFAULT '',    
+     countryCode    CHAR(3)   NOT NULL DEFAULT '',
+	 regionCode     CHAR(3)   NOT NULL DEFAULT '',
+	 datacenter     VARCHAR(30)   NOT NULL DEFAULT '',
+	 networkCCtag   VARCHAR(30)   NOT NULL DEFAULT '',
+	 linkspeed      VARCHAR(20)   NOT NULL DEFAULT '',
+	 fibremode      VARCHAR(20)   NOT NULL DEFAULT '',
+	 srcDemark      VARCHAR(20)   NOT NULL DEFAULT '',
+	 srcCage        VARCHAR(10)   NOT NULL DEFAULT '',
+	 srcCabinet     VARCHAR(20)   NOT NULL DEFAULT '',
+	 srcFloor       INT UNSIGNED  NOT NULL DEFAULT 1,
+	 srcNameSwitch  VARCHAR(20)   NOT NULL DEFAULT '',
+	 srcPort        INT UNSIGNED  NOT NULL DEFAULT 1,
+	 dstDemark      VARCHAR(20)   NOT NULL DEFAULT '',
+	 dstCabinet     VARCHAR(20)   NOT NULL DEFAULT '',
+	 dstFloor       INT UNSIGNED  NOT NULL DEFAULT 1,
+	 dstNameSwitch  VARCHAR(20)   NOT NULL DEFAULT '',
+	 dstPort        INT UNSIGNED  NOT NULL DEFAULT 1,
+	 serviceDate    VARCHAR(20)   NOT NULL DEFAULT '',
+	 decomDate VARCHAR(20)   NOT NULL DEFAULT '',
+     PRIMARY KEY (networkCCID)
+     ); 	 
